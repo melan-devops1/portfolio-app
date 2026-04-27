21a40e2ffe6df9b0d03343bb2bddb994aa15414e
melanieing <melanie0617@gmail.com>
2026-04-28 01:51:15 +0900

    crate docker compose yaml, edit jpa hibernate ddl-auto, create init multiple dbs script

---
+++ scripts/init-multiple-dbs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# PostgreSQL 컨테이너 초기 부팅 시 여러 DB를 한 번에 생성
+# POSTGRES_MULTIPLE_DATABASES 환경변수에서 콤마로 구분된 DB 이름들 읽음
+
+set -e
+set -u
+
+function create_user_and_database() {
+    local database=$1
+    echo "Creating database '$database'"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+        CREATE DATABASE $database;
+        GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+    echo "Multiple DB creation requested: $POSTGRES_MULTIPLE_DATABASES"
+    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+        create_user_and_database $db
+    done
+    echo "Multiple databases created"
+fi
